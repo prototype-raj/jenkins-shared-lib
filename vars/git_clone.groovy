851b9254dbcb8ce758aea9fe6d26bddc7f63ea5c
Rajarshi Rakshit <rjrshi1rakshit@gmail.com>
2025-07-02 18:57:36 +0530

    Update git_clone.groovy
    missed quotes

--- vars/git_clone.groovy
+++ vars/git_clone.groovy
@@ -1,5 +1,5 @@
 def call(String gitUrl, String branchName) {
   echo "This is cloning the code!"
   git url: "${gitUrl}", branch: "${branchName}"
-  echo "Code cloned successfully!!
+  echo "Code cloned successfully!!"
 }
