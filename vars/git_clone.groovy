58679bd052ad37f86aeae984b00a02dc81e9a237
Rajarshi Rakshit <rjrshi1rakshit@gmail.com>
2025-07-02 18:46:44 +0530

    Update git_clone.groovy
    corrected method parameters

--- vars/git_clone.groovy
+++ vars/git_clone.groovy
@@ -1,4 +1,4 @@
-def call(gitUrl, branchName) {
+def call(String gitUrl, String branchName) {
   echo "This is cloning the code!"
   git url: "${gitUrl}", branch: "${branchName}"
   echo "Code cloned successfully!!
